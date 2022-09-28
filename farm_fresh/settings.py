ebd92660adf34b31f20cc93d9cda43946b611693
Joao4569 <johnnyrod4569@gmail.com>
2022-09-28 14:56:14 +0000

    feat: Add allauth templates and base template

--- farm_fresh/settings.py
+++ farm_fresh/settings.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# Taken from boutique ado walkthrough project
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
