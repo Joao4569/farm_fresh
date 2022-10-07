ffe8b185741f9eeda691df58986f1de5a96036e6
Joao4569 <johnnyrod4569@gmail.com>
2022-10-07 08:38:02 +0000

    feat: Add product detail functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),  # Path to all products page
+    # Path to all products page
+    path('', views.all_products, name='products'),
+    # Path to product detail page
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
