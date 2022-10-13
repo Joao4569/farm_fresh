d9c99d4b7f51a5dac48595fa9d5c42e20591ff81
Joao4569 <johnnyrod4569@gmail.com>
2022-10-13 10:52:26 +0000

    feat: Add checkout success page logic

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    # Path to checkout page
+    path('', views.checkout, name='checkout'),
+
+    # Path to checkout success page
+    path('checkout_success/<order_number>',
+         views.checkout_success, name='checkout_success')
 ]
