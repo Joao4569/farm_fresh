800476201ca9210cb8969405c0437da86b9cf8c9
Joao4569 <johnnyrod4569@gmail.com>
2022-10-15 14:25:47 +0000

    chore: Remove secret key and debug

--- farm_fresh/settings.py
+++ farm_fresh/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#v*a$%bqcv!($+k87bz8np-)522t9*l2pva7y65%1tr_w$nb&-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'farm-fresh-ch.herokuapp.com', 'localhost'
