2d918cd692420e4aba20f0165cb414531eb31c69
Joao4569 <johnnyrod4569@gmail.com>
2022-10-15 16:27:50 +0000

    chore: Try second initial deployment attempt

--- farm_fresh/settings.py
+++ farm_fresh/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [
-    'farm-fresh-ch.herokuapp.com', 'localhost'
-]
+ALLOWED_HOSTS = ['farm-fresh-jch.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -138,17 +136,17 @@ WSGI_APPLICATION = 'farm_fresh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-# DATABASES = {
-    # 'default': dj_database_url.parse('')
- # }
-# else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
