9db1dfa6c90f7cd1a1cc193a13b16ed92f30e045
Joao4569 <johnnyrod4569@gmail.com>
2022-10-16 14:42:19 +0000

    fix: Fix minor bug found with include link name

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 
 from products.models import Product
@@ -20,10 +22,12 @@ def add_to_cart(request, item_id):
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(request, f'Updated {product.name} \
+            quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your shopping cart')
+        messages.success(request, f'Added {product.name} \
+            to your shopping cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -39,10 +43,12 @@ def adjust_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+        messages.success(request, f'Updated {product.name} \
+            quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your shopping cart')
+        messages.success(request, f'Removed {product.name} \
+            from your shopping cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -56,7 +62,8 @@ def remove_from_cart(request, item_id):
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your shopping cart')
+        messages.success(request, f'Removed {product.name} \
+            from your shopping cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
