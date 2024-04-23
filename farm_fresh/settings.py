057b36f89a792224ef71eed1459b42ac81e4e29e
joao4569 <johnnyrod4569@gmail.com>
2024-04-23 17:38:39 +0200

    chore: Add first Django unit test to Products/test_test.py, setup credentials in env.py for local postgres DB that is used for running tests in Django, keeping all credentials secure and stored locally.

--- farm_fresh/settings.py
+++ farm_fresh/settings.py
@@ -145,9 +145,14 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
+
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-testing-db',
+        'USER': os.environ.get('TESTING_DB_USER'),
+        'PASSWORD': os.environ.get('TESTING_DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
         }
     }
 
